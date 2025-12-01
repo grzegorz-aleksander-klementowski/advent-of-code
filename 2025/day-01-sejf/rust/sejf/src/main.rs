8229f81bcfb6b8d7d2f9cc8f3a3b1b28793b8c56
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-01 14:29:01 +0100

    chore: builded structure

--- 2025/day-01-sejf/rust/sejf/src/main.rs
+++ 2025/day-01-sejf/rust/sejf/src/main.rs
@@ -1,3 +1,28 @@
+struct DialZeroCounter(u32);
+
+struct SejfDialNum(u32);
+
+enum Direction {
+    Right,
+    Left,
+}
+
+struct Rotation {
+    direction: Direction,
+    distance: SejfDialNum,
+}
+
 fn main() {
     println!("Wesołych świąt!");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::SejfDialNum;
+
+    #[test]
+    #[should_panic(expected = "Out of scope")]
+    fn test_dial_num_scope() {
+        let sejf_num = SejfDialNum(100);
+    }
 }
