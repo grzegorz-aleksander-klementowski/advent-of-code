6545d32b0ac981dba71d17518ec9c91ff2a957a8
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-01 20:44:55 +0100

    feat: Added implementation for all of the structures

--- 2025/day-01-sejf/rust/sejf/src/main.rs
+++ 2025/day-01-sejf/rust/sejf/src/main.rs
@@ -1,8 +1,38 @@
-#[derive(Debug, PartialEq, Eq)]
-struct DialZeroCounter(u32);
+use std::fs;
+
+trait Input {
+    fn read_a_document(path: &str) -> Self;
+}
+
+struct TheAttachedDocument(String);
+
+impl Input for TheAttachedDocument {
+    fn read_a_document(path: &str) -> Self {
+        let file_content = fs::read_to_string(path).expect("Can't read file.");
+        Self(file_content)
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct SejfDialNum(u32);
+
+impl SejfDialNum {
+    fn rotate_sejf_dial(&mut self, rote: &Rotation) {
+        let sejf_dial_range = 100;
+        let distance = rote.distance.0 % sejf_dial_range;
+
+        match rote.direction {
+            Direction::Right => self.0 = (self.0 + distance) % sejf_dial_range,
+            Direction::Left => self.0 = (self.0 + sejf_dial_range - distance) % sejf_dial_range,
+        }
+    }
+}
+
+impl Default for SejfDialNum {
+    fn default() -> Self {
+        SejfDialNum(50)
+    }
+}
 
 enum Direction {
     Right,
@@ -12,6 +42,60 @@ enum Direction {
 struct Rotation {
     direction: Direction,
     distance: SejfDialNum,
+}
+
+impl Rotation {
+    fn new(direction: Direction, distance: SejfDialNum) -> Self {
+        Rotation {
+            direction,
+            distance,
+        }
+    }
+
+    fn parse(doc_snippet: String) -> Rotation {
+        let mut snippet_chars = doc_snippet.chars();
+
+        let direction = match snippet_chars.next().expect("Here should be a char!") {
+            'R' => Direction::Right,
+            'L' => Direction::Left,
+            _ => panic!(
+                "A wrong letter from the doc snippet. Should be a char `R` or `L`. The snippet: {doc_snippet:?}"
+            ),
+        };
+
+        let distance: u32 = snippet_chars
+            .as_str()
+            .parse()
+            .expect("Here should be a number");
+
+        Rotation::new(direction, SejfDialNum(distance))
+    }
+}
+
+impl Default for Rotation {
+    fn default() -> Self {
+        Rotation {
+            direction: Direction::Right,
+            distance: SejfDialNum(1),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct SejfDialZeroCounter(u32);
+
+impl Default for SejfDialZeroCounter {
+    fn default() -> Self {
+        SejfDialZeroCounter(0)
+    }
+}
+
+impl SejfDialZeroCounter {
+    fn count_zeros(&mut self, sejf_dial_num: &SejfDialNum) {
+        if sejf_dial_num.0 == 0 {
+            self.0 += 1;
+        }
+    }
 }
 
 fn main() {
