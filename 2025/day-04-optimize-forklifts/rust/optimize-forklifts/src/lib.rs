822e7c856407889cc5746a55990390a9e628406b
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-05 01:04:57 +0100

    test: added test to Grid, accorrding to TDD

--- 2025/day-04-optimize-forklifts/rust/optimize-forklifts/src/lib.rs
+++ 2025/day-04-optimize-forklifts/rust/optimize-forklifts/src/lib.rs
@@ -1,6 +1,7 @@
 mod grid {
     use crate::input::{Input, PathToDiagram};
 
+    #[derive(Debug, PartialEq, Eq)]
     struct Forklift;
     impl Forklift {
         fn parse(diagram_char: char) -> Option<Self> {
@@ -12,7 +13,7 @@ mod grid {
         }
     }
 
-    #[derive(Default)]
+    #[derive(Debug, PartialEq, Eq, Default)]
     struct Field(Option<Forklift>);
     impl Field {
         fn new(forklift: Option<Forklift>) -> Self {
@@ -20,18 +21,160 @@ mod grid {
         }
     }
 
+    #[derive(Debug, PartialEq, Eq)]
+    struct Grid(Vec<Vec<Field>>);
+    impl Grid {
+        fn parse(path: PathToDiagram) -> Self {
+            let content = Input::load(&path);
+            let mut grid_row: Vec<Field> = Vec::new();
+            let mut grid: Vec<Vec<Field>> = Vec::new();
+            /* for row in content.lines() {
+
+            } */
+            todo!()
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn test_parse_grid() {
+            let path = PathToDiagram("test".to_string());
+            let res = Grid::parse(path);
+            assert_eq!(
+                res,
+                Grid(vec![
+                    vec![
+                        Field(None),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None)
+                    ],
+                    vec![
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None)
+                    ],
+                    vec![
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift))
+                    ],
+                    vec![
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(Some(Forklift)),
+                        Field(None),
+                        Field(Some(Forklift)),
+                        Field(None)
+                    ],
+                ])
+            );
+        }
+    }
+
     struct GridArea {
         height: usize,
         weight: usize,
         field: Field,
-    }
-
-    struct Grid(Vec<Vec<Field>>);
-    impl Input for GridArea {
-        fn load(&self) -> String {
-            let path = PathToDiagram(String::from("input"));
-            todo!()
-        }
     }
 }
 
